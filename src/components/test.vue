e931ca15dd0a631efc0b3464ee64d6b75b1dfa26
huhuhahaka <1739660721@qq.com>
2020-11-28 17:19:22 +0800

    添加练习文件

---
+++ src/components/test.vue
@@ -0,0 +1,29 @@
+<template>
+	<h1 class="red">大家好！！！ {{msg}} {{name}}</h1>
+</template>
+
+<script>
+	export default {
+		name: 'test',
+		props: {
+			msg: {
+				type: String,
+				default: "我是"
+			}
+		},
+		methods: {
+
+		},
+		data() {
+			return {
+				name: "Jason"
+			}
+		}
+	}
+</script>
+
+<style>
+	.red {
+		color: red;
+	}
+</style>
